5fd47630693198ad3109c5e31c0a5ea46d62bb04
Mike MacDougall <mgmacdougall@outlook.com>
2026-06-27 16:24:04 -0400

    Initial add of StepsList component.

--- client/src/pages/TestCases/List/components/StepsList..jsx
+++ client/src/pages/TestCases/List/components/StepsList..jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function StepsList.() {
-  return (
-    <div>StepsList.</div>
-  )
+function StepsList() {
+  return <div>StepsList</div>;
 }
 
-export default StepsList.
+export default StepsList;
